51458af756d2264a5464bffb2dd5e425575f2c83
Justin Shaw <shaw.justin@shaw.ca>
2026-04-24 14:17:05 -0600

    add POST mapping for events

--- backend/src/main/java/com/code/backend/events/EventsController.java
+++ backend/src/main/java/com/code/backend/events/EventsController.java
@@ -1,8 +1,7 @@
 package com.code.backend.events;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,5 +16,9 @@ public class EventsController {
         return eventRepository.findAll();
     }
 
+    @PostMapping("/events")
+    public Events createBook(@RequestBody Events events) {
+        return eventRepository.save(events);
+    }
 
 }
