1518234f6587f9c77d890a496025bf59bcf9fe5e
Justin Shaw <shaw.justin@shaw.ca>
2026-04-24 13:58:55 -0600

    add Event class and EventController

---
+++ backend/src/main/java/com/code/backend/Event.java
@@ -0,0 +1,87 @@
+package com.code.backend;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String director;
+    private double capacity;
+    private String image_url;
+
+    public Event() {}
+
+    public Event(String title, String description, String director,
+                 double capacity, String image_url) {
+        this.title = title;
+        this.description = description;
+        this.director = director;
+        this.capacity = capacity;
+        this.image_url = image_url;
+    }
+
+    public String getId() {
+        return id.toString();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(double capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", director='" + director + '\'' +
+                ", capacity=" + capacity +
+                ", image_url='" + image_url + '\'' +
+                '}';
+    }
+}
---
+++ backend/src/main/java/com/code/backend/EventRepository.java
@@ -0,0 +1,8 @@
+package com.code.backend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    // any custom finders go here
+}
