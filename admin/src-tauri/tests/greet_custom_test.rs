49d7853ea2a871d6d7f4a1ff89e26418f1ee9ba5
Justin Shaw <shaw.justin@shaw.ca>
2026-05-04 13:29:30 -0600

    remove rust test for component that does not exist

--- admin/src-tauri/tests/greet_custom_test.rs
+++
@@ -1,9 +0,0 @@
-use admin_lib::greet_custom;
-
-#[test]
-fn test_greet_custom() {
-    assert_eq!(
-        greet_custom("Justin"),
-        "Hello, Justin! You've been greeted from Rust!"
-    );
-}
