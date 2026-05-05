78fd6f648540d3fc933390d9dd22384fb6ed7b52
Justin Shaw <shaw.justin@shaw.ca>
2026-05-05 10:31:58 -0600

    add tauri window state remembering on startup

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
 import NewEventForm from './components/forms/NewEventForm.vue';
 import NewPatronForm from './components/forms/NewPatronForm.vue';
+import { onMounted } from "vue";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 import { SharedButton } from '@theater/shared'
+
+// Allow Tauri to show window again after resize is complete
+onMounted(async () => {
+  await getCurrentWindow().show()
+})
+
 
 // async function greet() {
 //   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
