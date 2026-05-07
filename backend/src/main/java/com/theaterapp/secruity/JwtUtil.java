1bef34199ff979f0cdb19622e9c320a73c07ab49
Justin Shaw <shaw.justin@shaw.ca>
2026-05-07 11:28:36 -0600

    add JWT Util Class

---
+++ backend/src/main/java/com/theaterapp/secruity/JwtUtil.java
@@ -0,0 +1,73 @@
+package com.theaterapp.secruity;
+
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtUtil {
+
+    @Value("${jwt.secrete}")
+    private String jwt_secrete;
+
+    @Value("${jwt.expiration}")
+    private int jwt_expiration;
+
+    private SecretKey key;
+
+    @PostConstruct
+    public void init() {
+        this.key =
+                Keys.hmacShaKeyFor(jwt_secrete.getBytes(StandardCharsets.UTF_8));
+    }
+
+    /**
+     *
+     * @param email
+     * @return
+     */
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date((new Date().getTime() + jwt_expiration)))
+                .signWith(key)
+                .compact();
+    }
+
+    /**
+     *
+     * @param token
+     * @return
+     */
+    public String getUserFromToken(String token) {
+        return Jwts.parser().verifyWith(key).build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    /**
+     *
+     * @param token
+     * @return
+     */
+    public boolean validateJWTtoken(String token) {
+        try {
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(token);
+            return true;
+        } catch (Exception e) {
+            log.error("JWT validation error: {}", e.getMessage());
+        }
+        return false;
+    }
+}
