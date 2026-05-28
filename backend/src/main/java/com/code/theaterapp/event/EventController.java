2d96908c2e3ffd613816e30d82dd2b8cbcfd91f1
Justin Shaw <shaw.justin@shaw.ca>
2026-05-28 09:49:17 -0600

    add plural to events and tickets for API routing

--- backend/src/main/java/com/code/theaterapp/event/EventController.java
+++ backend/src/main/java/com/code/theaterapp/event/EventController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/event")
+@RequestMapping(value = "/events")
 public class EventController {
 
     @GetMapping("/")
--- backend/src/main/java/com/code/theaterapp/tickets/TicketController.java
+++ backend/src/main/java/com/code/theaterapp/tickets/TicketController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/ticket")
+@RequestMapping(value = "/tickets")
 public class TicketController {
 
     @GetMapping("/")
