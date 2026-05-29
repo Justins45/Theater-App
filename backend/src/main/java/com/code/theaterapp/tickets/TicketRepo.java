1146d3e724190f9fd96f644f466b8115c0084c82
Justin Shaw <shaw.justin@shaw.ca>
2026-05-28 18:00:13 -0600

    add TicketRepo

---
+++ backend/src/main/java/com/code/theaterapp/tickets/TicketRepo.java
@@ -0,0 +1,8 @@
+package com.code.theaterapp.tickets;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketRepo extends JpaRepository<Ticket, Long> {
+}
