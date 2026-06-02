12575bf4a5546ad5bceb9e342d1b8928c7397918
Justin Shaw <shaw.justin@shaw.ca>
2026-06-02 14:43:57 -0600

    add annotation checks on Login / register DTO's

--- backend/src/main/java/com/code/theaterapp/auth/dtos/LoginRequestDTO.java
+++ backend/src/main/java/com/code/theaterapp/auth/dtos/LoginRequestDTO.java
@@ -1,7 +1,22 @@
 package com.code.theaterapp.auth.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record LoginRequestDTO(
+
+        // TODO: Change to email based login (removes Username - seperate branch + PR)
+
+        // TODO: make branch for adding custom annotations
+
+        @NotBlank
+        @Size(min = 3, max = 50)
+        @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Username may only contain letters, numbers, underscores, and hyphens")
         String username,
+
+        @NotBlank
+        @Size(min = 8,max = 72)
         String password
 ) {
 }
