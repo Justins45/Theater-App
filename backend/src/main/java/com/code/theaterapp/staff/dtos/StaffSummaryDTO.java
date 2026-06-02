25002f2eada29456a7b68e7fe4a8e05e285da1ad
Justin Shaw <shaw.justin@shaw.ca>
2026-06-02 12:32:15 -0600

    add StaffSummaryDTO

---
+++ backend/src/main/java/com/code/theaterapp/staff/dtos/StaffSummaryDTO.java
@@ -0,0 +1,14 @@
+package com.code.theaterapp.staff.dtos;
+
+import com.code.theaterapp.shared.enums.Role;
+
+import java.util.UUID;
+
+public record StaffSummaryDTO(
+        UUID id,
+        String username,
+        String firstName,
+        String lastName,
+        Role role
+) {
+}
