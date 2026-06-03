2dcbb2682be1cb35f422fb32b09e7fe56bf6f50b
Justin Shaw <shaw.justin@shaw.ca>
2026-06-03 10:53:19 -0600

    add validate no staff account exists

--- backend/src/main/java/com/code/theaterapp/shared/person/PersonValidationService.java
+++ backend/src/main/java/com/code/theaterapp/shared/person/PersonValidationService.java
@@ -1,6 +1,9 @@
 package com.code.theaterapp.shared.person;
 
+import com.code.theaterapp.exceptions.AccountAlreadyExistsException;
 import com.code.theaterapp.exceptions.EmailAlreadyExistsException;
+import com.code.theaterapp.exceptions.EntityNotFoundException;
+import com.code.theaterapp.staff.StaffRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,23 +12,21 @@ import org.springframework.stereotype.Service;
 public class PersonValidationService {
 
     private final PersonRepo personRepo;
-
-    /*
-     *  TODO: Things to make
-     *
-     *   PATRON CHECKS
-     *  1. check if email is taken
-     *
-     *  STAFF CHECKS
-     *  1. Check Person Exists (brand new account)
-     *  2. Return person to add Staff account to (existing account)
-     *  3. check if staff account exists
-     */
-
+    private final StaffRepo staffRepo;
 
     public void validateUniqueEmail(String email) {
         personRepo.findByEmail(email).ifPresent(
                 p -> { throw new EmailAlreadyExistsException(email); }
+        );
+    }
+
+    public void validateNoStaffAccount(String email) {
+        Person person = personRepo.findByEmail(email).orElseThrow(
+                () -> new EntityNotFoundException("Email not found")
+        );
+
+        staffRepo.findByPerson(person).ifPresent(
+                s -> { throw new AccountAlreadyExistsException("Staff account already exists"); }
         );
     }
 
