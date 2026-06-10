b2d617ce5504e258413589c1467b23806584e70f
Justin Shaw <shaw.justin@shaw.ca>
2026-06-10 14:45:41 -0600

    add displayment of data inside component

---
+++ frontend/src/components/EventSummaryItem.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+defineProps({
+    id: String,
+    title: String,
+    showTime: String,
+    stageName: String,
+})
+</script>
+
+<template>
+  <div class="event">
+    <RouterLink :to="'events/' + id">
+      <h3>{{ title }}</h3>
+      <p>Show Time: {{ showTime }}</p>
+      <p>Playing at {{ stageName }}</p>
+    </RouterLink>
+  </div>
+</template>
+
+<style scoped lang="scss">
+h3, p {
+  margin: 0;
+  padding: 0;
+}
+a {
+  color: black;
+  text-decoration: none;
+}
+</style>
--- frontend/src/pages/events/index.vue
+++ frontend/src/pages/events/index.vue
@@ -1,19 +1,32 @@
 <script setup lang="ts">
+import EventSummaryItem from '@/components/EventSummaryItem.vue'
 // get data from api events
-const data = {
-  "id": "019eacb8-f6e4-77da-afda-86d103b67915",
-  "title": "First Event yay!",
-  "showTime": "17:15",
-  "stageId": 1,
-  "stageName": "main stage"
-}
+const data = [
+  {
+    "id": "019eacb8-f6e4-77da-afda-86d103b67915",
+    "title": "First Event yay!",
+    "showTime": "17:15",
+    "stageId": 1,
+    "stageName": "main stage"
+  },
+  {
+    "id": "019eb33d-aafe-716c-99c3-d65c5d2cfe67",
+    "title": "Cat Sitting the Musical!",
+    "showTime": "09:55",
+    "stageId": 1,
+    "stageName": "main stage"
+  }
+]
 </script>
 
 <template>
   <div>
-    <h2>Temp data</h2>
-    <pre>{{ data }}</pre>
+    <h2>Events</h2>
+    <div v-for="item in data" :key="item.id" class="event">
+        <EventSummaryItem v-bind="item"></EventSummaryItem>
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+</style>
