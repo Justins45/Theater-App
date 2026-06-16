28e9a3cae3d3036f49484c2960aaa31779669903
Justin Shaw <shaw.justin@shaw.ca>
2026-06-16 10:40:30 -0600

    add CSRF default cookie names to axios

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,5 +2,9 @@ import axios from "axios"
 
 export const api = axios.create({
   baseURL: 'http://localhost:8080',
-  withCredentials: true,
+  withCredentials: true,           // sends cookies cross-origin
+
+  // CSRF Cookies - (default names - change if server changes from defaults)
+  xsrfCookieName: 'XSRF-TOKEN',
+  xsrfHeaderName: 'X-XSRF-TOKEN',
 })
