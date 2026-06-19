1230a5be3c153ebf3c40913160f863a0d135e4d1
Justin Shaw <shaw.justin@shaw.ca>
2026-06-19 12:20:05 -0600

    move showtime to performance and add PerformanceStatus ENUM

--- backend/src/main/java/com/code/theaterapp/performance/Performance.java
+++ backend/src/main/java/com/code/theaterapp/performance/Performance.java
@@ -1,6 +1,7 @@
 package com.code.theaterapp.performance;
 
 import com.code.theaterapp.event.Event;
+import com.code.theaterapp.shared.enums.PerformanceStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,6 +11,8 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.time.Instant;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 @Entity
@@ -25,6 +28,13 @@ public class Performance {
     @UuidGenerator(style = UuidGenerator.Style.VERSION_7)
     private UUID id;
 
+    @Column(nullable = false)
+    private LocalTime showTime;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PerformanceStatus performanceStatus;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "event_id")
     private Event event;
@@ -32,4 +42,10 @@ public class Performance {
     @CreationTimestamp
     @Column(nullable = false)
     private Instant createdAt;
+
+    public String getWallClock() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a");
+        return this.showTime.format(formatter);
+    }
+
 }
