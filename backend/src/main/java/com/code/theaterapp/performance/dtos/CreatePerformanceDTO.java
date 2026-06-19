a783886ccc748dc3bc0d0a44129c41533f25378a
Justin Shaw <shaw.justin@shaw.ca>
2026-06-19 13:40:19 -0600

    add create performance DTO

--- backend/src/main/java/com/code/theaterapp/performance/dtos/CreatePerformanceDTO.java
+++ backend/src/main/java/com/code/theaterapp/performance/dtos/CreatePerformanceDTO.java
@@ -1,10 +1,8 @@
 package com.code.theaterapp.performance.dtos;
 
 import java.time.LocalTime;
-import java.util.UUID;
 
 public record CreatePerformanceDTO(
-        LocalTime showTime,
-        UUID event_id
+        LocalTime showTime
 ) {
 }
