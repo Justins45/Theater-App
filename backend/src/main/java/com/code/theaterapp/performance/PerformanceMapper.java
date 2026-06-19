7809bfd2eebe861c3454e020bd240cd2c8ce2c12
Justin Shaw <shaw.justin@shaw.ca>
2026-06-19 13:53:18 -0600

    add mapper to convert to performance Summary

--- backend/src/main/java/com/code/theaterapp/performance/PerformanceMapper.java
+++ backend/src/main/java/com/code/theaterapp/performance/PerformanceMapper.java
@@ -1,6 +1,7 @@
 package com.code.theaterapp.performance;
 
 import com.code.theaterapp.performance.dtos.PerformanceDetailsDTO;
+import com.code.theaterapp.performance.dtos.PerformanceSummaryDTO;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +9,15 @@ public class PerformanceMapper {
 
     public PerformanceDetailsDTO toDetails(Performance performance) {
         return new PerformanceDetailsDTO(
+                performance.getId(),
+                performance.getWallClock(),
+                performance.getPerformanceStatus(),
+                performance.getEvent().getId()
+        );
+    }
+
+    public PerformanceSummaryDTO toSummary(Performance performance) {
+        return new PerformanceSummaryDTO(
                 performance.getId(),
                 performance.getWallClock(),
                 performance.getPerformanceStatus(),
