3bd58d82359029d2a47c11150865a9a04bb5bcff
Justin Shaw <shaw.justin@shaw.ca>
2026-06-22 11:36:16 -0600

    update event seating controller

--- backend/src/main/java/com/code/theaterapp/seating/event_seating/EventSeatingController.java
+++ backend/src/main/java/com/code/theaterapp/seating/event_seating/EventSeatingController.java
@@ -1,22 +1,22 @@
 package com.code.theaterapp.seating.event_seating;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 // Use events URL to get the pathing ID for the Event
-@RequestMapping(value = "/performances")
+@RequestMapping(value = "/events")
 public class EventSeatingController {
 
     private final EventSeatingRepo eventSeatingRepo;
-    private final String URL_PATH = "/{performanceId}/seating";
+    private final String URL_PATH = "/{eventId}/performances/{performanceId}/seating";
 
     // get performances for an event
     @GetMapping(URL_PATH)
