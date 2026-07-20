7aa46cb15cb1395ae512ad25aa5de5bc11e74c50
Justin Shaw <shaw.justin@shaw.ca>
2026-07-20 09:55:18 -0600

    Merge: add-delete-cartItem into main (#31)
    * add delete for cart items
    
    * add delete API route to frontend
    
    * add proper delete function in backend

--- backend/src/main/java/com/code/theaterapp/shoppingCart/cart/CartService.java
+++ backend/src/main/java/com/code/theaterapp/shoppingCart/cart/CartService.java
@@ -100,4 +100,11 @@ public class CartService {
         // TODO: TEMP METHOD FOR OTHER STUFF BEFORE IT
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
     }
+
+    // 204 - NO CONTENT (deleted success) | 404 NOT FOUND (deleted failed)
+    public ResponseEntity<Void> removeItemFromCart(UUID itemId, UUID cartId) {
+        return cartItemRepo.deleteByIdAndCartId(itemId, cartId) > 0
+                ? ResponseEntity.status(HttpStatus.NO_CONTENT).build()
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
 }
--- backend/src/main/java/com/code/theaterapp/shoppingCart/cartItem/CartItemRepo.java
+++ backend/src/main/java/com/code/theaterapp/shoppingCart/cartItem/CartItemRepo.java
@@ -2,6 +2,7 @@ package com.code.theaterapp.shoppingCart.cartItem;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -9,4 +10,8 @@ import java.util.UUID;
 @Repository
 public interface CartItemRepo extends JpaRepository<CartItem, UUID> {
     List<CartItem> findAllByCartId(UUID cartId);
+
+    // TODO: rename or make others when adding Merch
+    @Transactional
+    int deleteByIdAndCartId(UUID id, UUID cartId);
 }
