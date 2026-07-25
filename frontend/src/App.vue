8ed4116096c538c443c512ad562c7ffbf990ad07
Justin Shaw <shaw.justin@shaw.ca>
2026-07-25 16:29:45 -0600

    Merge add-cart-page into main (#34)
    * move cart to its own cart page
    
    * remove unused imports
    
    * add checkout button
    
    * add comment about Stripe integration

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { SharedButton } from "@theater/shared";
 import { RouterLink, RouterView } from "vue-router";
-import ShoppingCart from '@/components/ShoppingCart.vue'
 import { useCartStore } from '@/stores/cart'
 
 const cartStore = useCartStore()
@@ -17,11 +16,11 @@ cartStore.loadCart()
       <li><RouterLink to="/account/login">Login</RouterLink></li>
       <li><RouterLink to="/account/register">Register</RouterLink></li>
       <li><RouterLink to="/account/me">My Account</RouterLink></li>
+      <li><RouterLink to="/cart">Cart<span v-if="cartStore.totalItems > 0"> - {{ cartStore.totalItems }}</span></RouterLink></li>
     </ul>
 
   </nav>
   <SharedButton />
-  <ShoppingCart />
   <router-view></router-view>
 </template>
 
---
+++ frontend/src/pages/cart/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import ShoppingCart from '@/components/ShoppingCart.vue'
+import { useCartStore } from '@/stores/cart'
+
+const cartStore = useCartStore()
+cartStore.loadCart()
+
+async function startCheckout() {
+  console.log('start checkout initiated')
+}
+
+</script>
+
+<template>
+  <div class="cart">
+    <ShoppingCart />
+    <div v-if="cartStore.totalItems > 0">
+      <!-- TODO: Stripe checkout process
+        (1) - Link to a checkout page
+        (2) - Server generates correct total amount to pay
+        (3) - Stripe inside a <iframe> builds the form and the rest of the checkout
+        (4) - Sends back a conformation I take and create a Ticket
+        (5) - idk what else, but will know when at this point
+      -->
+      <button type="button" name="checkout" @click="startCheckout">Checkout</button>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.cart {
+  margin: 1rem 0;
+}
+</style>
