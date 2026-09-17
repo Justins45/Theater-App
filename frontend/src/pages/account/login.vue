d0970d5eea481e00537630506470a521ff612152
Justin S <shaw.justin@shaw.ca>
2026-09-17 16:25:35 -0600

    re format login page

--- frontend/src/pages/account/login.vue
+++ frontend/src/pages/account/login.vue
@@ -27,32 +27,45 @@ async function handleLogin() {
 <template>
 <div>
   <h1>Please login</h1>
-  <form @submit.prevent="handleLogin">
-    <label for="email">Email</label>
-    <input
-      id="email"
-      name="email"
-      type="email"
-      v-model="loginInfo.email"
-      placeholder="email@example.ca"
-      autocomplete="email"
-      required
-    />
-    <input
-      id="password"
-      name="password"
-      type="password"
-      v-model="loginInfo.password"
-      placeholder="password"
-      autocomplete="current-password"
-      required
-    />
-    <button type="submit">Login</button>
-  </form>
+  <div class="login-form">
+    <form @submit.prevent="handleLogin">
+      <div class="email">
+        <label for="email">Email</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          v-model="loginInfo.email"
+          placeholder="email@example.ca"
+          autocomplete="email"
+          required
+        />
+      </div>
+      <div class="password">
+        <label for="password">Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          v-model="loginInfo.password"
+          placeholder="password"
+          autocomplete="current-password"
+          required
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
   <p v-if="responseMessage">{{ responseMessage }}</p>
 </div>
 </template>
 
 <style scoped lang="scss">
 
+.email, .password {
+  display: flex;
+  flex-direction: column;
+  width: 15rem;
+  margin-bottom: 1rem;
+}
 </style>
